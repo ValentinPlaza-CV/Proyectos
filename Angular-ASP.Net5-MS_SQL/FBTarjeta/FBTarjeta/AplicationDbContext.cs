638457a9307ecd5a96e8959ca952c6e99ec7781e
ValentinPlaza-CV <valenplaza1@gmail.com>
2021-06-12 15:16:55 -0300

    V1.0.0

---
+++ Angular-ASP.Net5-MS_SQL/FBTarjeta/FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCreditos { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
